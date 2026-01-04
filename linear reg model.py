e65f4c75659b4c09b012e1611eae29244ed88c6d
Miriampally Venkata Sugunadithya <venkatasugunadithya@gmail.com>
2026-01-04 22:46:39 +0530

    Refactor params function and enhance input validation
    Added return statement to params function and improved error handling.

--- linear reg model.py
+++ linear reg model.py
@@ -12,4 +12,20 @@ def params (x_train, y_train):
      A=np.array([[len(x_train), sigma_x], [sigma_x, sigma_x2]])
      B=np.array([sigma_y, sigma_xy])
      solution = np.linalg.solve(A, B)
-     print(solution)
+    return solution
+
+if __name__ == "__main__":
+     try:
+          xt=list(map(int,input("enter space saperated features for the ml model\n").split()))
+          yt=list(map(int,input("enter the space saperated targets for the ml model\n").split()))
+          if(len(xt)!=len(yt)):
+               sys.exit("the number of features is not equal to number of targets")
+     except Exception as e:
+          print("the following execption has occured:\ne")
+     else:
+          res=params(xt,yt)
+          x_test=float(input("enter a value you want to test for....\nenter 0 to exit\n"))
+          while(x_test!=0):
+               print("expected result is: ", res[0]+(x_test*res[1]))
+               x_test=float(input("enter a value you want to test for....\nenter 0 to exit\n"))
+
